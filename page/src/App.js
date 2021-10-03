6d8d2d405c89b1999436a8e67d36b7310e1a5157
fastsyrup <mike.j.solan@gmail.com>
2021-10-03 22:53:05 +0200

    firebase first steps

--- page/src/App.js
+++ page/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from "styled-components";
+import * as firebase from './firebase';
+
+const InnerContainer = styled.div`
+  display: flex;
+  max-width: 800px;
+  width: 100%;
+  flex-wrap: wrap;
+`;
+
+const OuterContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+`;
 
 function App() {
+  const [voteData, setVoteData] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const dataShell = await firebase.getVotesList();
+      const data = dataShell.data();
+      console.log(data.fields);
+    })()
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <OuterContainer>
+        <InnerContainer>
+          Blä2
+        </InnerContainer>
+      </OuterContainer>
     </div>
   );
 }
